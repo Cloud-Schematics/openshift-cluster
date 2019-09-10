a650d5a122b921f0fd4bc26153fb33ac68d09a41
hkantare@in.ibm.com <hkantare@in.ibm.com>
2019-09-10 14:38:41 +0530

    Add templates for IKS openshift cluster

---
+++ variables.tf
@@ -0,0 +1,26 @@
+variable "ibmcloud_api_key" {}
+variable "machine_type" {
+   default = "b2c.8x32"
+}
+variable "hardware" {
+   default = "shared"
+}
+
+variable "datacenter" {
+  default = "wdc07"
+}
+
+variable "default_pool_size" {
+  default = "2"
+}
+
+variable "private_vlan_id" {}
+
+variable "public_vlan_id" {}
+
+variable "cluster_name" {
+  default = "cluster"
+}
+variable kube_version {
+  default = "3.11_openshift"
+}
